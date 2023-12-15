5874473d4239609d98750fc23da93c77ab1a65a7
Eduardo Botelho <eduardo11224b@gmail.com>
2023-12-15 23:03:27 +0000

    feat: Make 404 error page and route(nothing redirects to it yet thought)

--- server/controllers/index.go
+++ server/controllers/index.go
@@ -8,8 +8,13 @@ import (
 
 func SetIndexRoutes(e *echo.Echo) {
 	e.GET("/", GetIndexController)
+	e.GET("/404", Get404Controller)
 }
 
 func GetIndexController(c echo.Context) error {
 	return c.Render(http.StatusOK, "index", nil)
+}
+
+func Get404Controller(c echo.Context) error {
+	return c.Render(http.StatusOK, "404", nil)
 }
