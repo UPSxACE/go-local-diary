5359707cf0ccc229f949c3de16a6ad94f507ecb6
Eduardo Botelho <eduardo11224b@gmail.com>
2024-01-31 23:05:56 +0000

    test: Simplify app package test and improve it

--- app/app_test.go
+++ app/app_test.go
@@ -4,292 +4,151 @@ import (
 	"html/template"
 	"reflect"
 	"testing"
+
+	"github.com/UPSxACE/go-local-diary/utils/testhelper"
 )
 
 func TestDefaultFuncList(t *testing.T) {
-	type TestCase struct {
-		input  []interface{}
-		output []interface{}
-	}
+	type Input = []any
+	type Output = []any
 
-	// Defining test cases
-	testCases := []TestCase{
-		{
-			input:  []interface{}{"item1", "item2", "item3"},
-			output: []interface{}{"item1", "item2", "item3"},
-		},
-		{
-			input:  []interface{}{1, 2, 3, 4},
-			output: []interface{}{1, 2, 3, 4},
-		},
-		{
-			input:  []interface{}{true, false, true, false},
-			output: []interface{}{true, false, true, false},
-		},
-		{
-			input:  []interface{}{map[string]string{"key1": "val1", "key2": "val2"}},
-			output: []interface{}{map[string]string{"key1": "val1", "key2": "val2"}},
-		},
-	}
+	th := testhelper.CreateTestHelper[Input, Output]()
 
-	// Running the test cases
-	for _, tc := range testCases {
-		output := list(tc.input...)
+	th.AddTestcase([]any{"item1", "item2", "item3"},nil)
+	th.AddTestcase([]any{1, 2, 3, 4}, nil)
+	th.AddTestcase([]any{true, false, true, false},nil)
+	th.AddTestcase([]interface{}{map[string]string{"key1": "val1", "key2": "val2"}}, nil)
 
-		// Check if the output matches the expected result
-		if !reflect.DeepEqual(tc.output, output) {
-			t.Errorf("Expected %v, got %v", tc.output, output)
-		}
+	for _, input := range th.GetInputs() {
+		output := list(input...)
+		testhelper.ExpectEqual(t, input, output)
 	}
 }
 
 func TestDefaultFuncObj(t *testing.T) {
-	type TestCase struct {
-		input  string
-		output map[string]string
-	}
+	th := testhelper.CreateTestHelper[string,any]()
 
-	// Defining test cases
-	testCases := []TestCase{
-		{
-			input:  "key1:val1,,key2:val2",
-			output: map[string]string{"key1": "val1", "key2": "val2"},
-		},
-		{
-			input:  "ss:this is a sentence,,com:sentence with, comma!",
-			output: map[string]string{"ss": "this is a sentence", "com": "sentence with, comma!"},
-		},
-	}
+	th.AddTestcase("key1:val1,,key2:val2", map[string]string{"key1": "val1", "key2": "val2"})
+	th.AddTestcase("ss:this is a sentence,,com:sentence with, comma!", map[string]string{"ss": "this is a sentence", "com": "sentence with, comma!"})
 
-	// Running the test cases
-	for _, tc := range testCases {
-		output := obj(tc.input)
-
-		// Check if the output matches the expected result
-		if !reflect.DeepEqual(tc.output, output) {
-			t.Errorf("Expected %v, got %v", tc.output, output)
-		}
+	for index, input := range th.GetInputs() {
+		output := obj(input)
+		testhelper.ExpectEqual(t, output, th.GetOutput(index))
 	}
 }
 
 func TestDefaultFuncObjError(t *testing.T) {
-	type TestCase struct {
-		input   string
-		errCode int
-	}
+	type Input = string;
+	var ExpectedErrorCode = 1
 
-	// Defining test cases
-	testCases := []TestCase{
-		{
-			input:   "key1::val1,,key2:val2",
-			errCode: 1,
-		},
-		{
-			input:   "ss:this is a sentence,com:sentence with, comma!",
-			errCode: 1,
-		},
-		{
-			input:   "ac",
-			errCode: 1,
-		},
-	}
+	th := testhelper.CreateTestHelper[Input, any]()
+	th.AddTestcase("key1::val1,,key2:val2", nil)
+	th.AddTestcase("ss:this is a sentence,com:sentence with, comma!", nil)
+	th.AddTestcase("ac", nil)
 
-	// Running the test cases
-	for _, tc := range testCases {
-		func() {
-			defer func(code int) {
-				r := recover()
-				if r == nil {
-					t.Errorf("The code did not panic")
+	for _, input := range th.GetInputs() {
+		didPanic, r := testhelper.ExpectPanic(t, func(){
+			obj(input)
+		})
+		if(didPanic){
+			correctType := reflect.TypeOf(r) == reflect.TypeOf(&DefMapInvalidArgs{})
+			if (!correctType) {
+				t.Fatalf("Not the expected error type")
+			} 
+			if (correctType){
+				rConverted, ok := r.(*DefMapInvalidArgs)
+
+				if !ok {
+					t.Fatalf("Conversion to the correct error type in defer function failed")
 				}
-
-				if reflect.TypeOf(r) != reflect.TypeOf(&DefMapInvalidArgs{}) {
-					t.Errorf("Not the expected error type")
-				} else {
-					rConverted, ok := r.(*DefMapInvalidArgs)
-
-					if !ok {
-						t.Errorf("Conversion to the correct error type in defer function failed")
-					}
-
-					if ok && rConverted.code != code {
-						t.Errorf("Panicked with the wrong code")
-					}
+				
+				if ok && rConverted.code != ExpectedErrorCode {
+					t.Fatalf("Panicked with the wrong code")
 				}
-			}(tc.errCode)
-			obj(tc.input)
-		}()
+			}
+		}
 	}
 }
 
 func TestDefaultFuncSum(t *testing.T) {
-	type TestCase struct {
-		input  [2]int
-		output int
-	}
+	th := testhelper.CreateTestHelper[[2]int, int]()
 
-	// Defining test cases
-	testCases := []TestCase{
-		{
-			input:  [2]int{1, 2},
-			output: 3,
-		},
-		{
-			input:  [2]int{4,5},
-			output: 9,
-		},
-		{
-			input:  [2]int{-1, 2},
-			output: 1,
-		},
-		{
-			input:  [2]int{-1, -5},
-			output: -6,
-		},
-		{
-			input:  [2]int{0,0},
-			output: 0,
-		},
-	}
+	th.AddTestcase([2]int{1, 2},3)
+	th.AddTestcase([2]int{4, 5},9)
+	th.AddTestcase([2]int{-1, 2},1)
+	th.AddTestcase([2]int{-1, -5},-6)
+	th.AddTestcase([2]int{0, 0},0)
 
-	// Running the test cases
-	for _, tc := range testCases {
-		output := sum(tc.input[0], tc.input[1])
+	for index, input := range th.GetInputs() {
+		output := sum(input[0], input[1])
 
-		// Check if the output matches the expected result
-		if !reflect.DeepEqual(tc.output, output) {
-			t.Errorf("Expected %v, got %v", tc.output, output)
-		}
+		testhelper.ExpectEqual(t, output, th.GetOutput(index))
 	}
 }
 
 func TestDefaultFuncSumStr(t *testing.T) {
-	type TestCase struct {
-		input  [2]string
-		output int
-	}
+	th := testhelper.CreateTestHelper[[2]string, int]()
 
-	// Defining test cases
-	testCases := []TestCase{
-		{
-			input:  [2]string{"1", "2"},
-			output: 3,
-		},
-		{
-			input:  [2]string{"4","5"},
-			output: 9,
-		},
-		{
-			input:  [2]string{"-1", "2"},
-			output: 1,
-		},
-		{
-			input:  [2]string{"-1", "-5"},
-			output: -6,
-		},
-		{
-			input:  [2]string{"0","0"},
-			output: 0,
-		},
-	}
+	th.AddTestcase([2]string{"1", "2"},3)
+	th.AddTestcase([2]string{"4", "5"},9)
+	th.AddTestcase([2]string{"-1", "2"},1)
+	th.AddTestcase([2]string{"-1", "-5"},-6)
+	th.AddTestcase([2]string{"0", "0"},0)
 
-	// Running the test cases
-	for _, tc := range testCases {
-		output := sumStr(tc.input[0], tc.input[1])
+	for index, input := range th.GetInputs() {
+		output := sumStr(input[0], input[1])
 
-		// Check if the output matches the expected result
-		if !reflect.DeepEqual(tc.output, output) {
-			t.Errorf("Expected %v, got %v", tc.output, output)
-		}
+		testhelper.ExpectEqual(t, output, th.GetOutput(index))
 	}
 }
 
 func TestDefaultFuncSumStrError(t *testing.T) {
-	type TestCase struct {
-		input  [2]string
-		errCode int
-	}
+	type Input = [2]string;
+	var ExpectedErrorCode = 2
 
-	// Defining test cases
-	testCases := []TestCase{
-		{
-			input:  [2]string{"1.2", "2"},
-			errCode: 2,
-		},
-		{
-			input:  [2]string{"4.9","5"},
-			errCode: 2,
-		},
-		{
-			input:  [2]string{"-1", "2,"},
-			errCode: 2,
-		},
-		{
-			input:  [2]string{"abc", "-5"},
-			errCode: 2,
-		},
-		{
-			input:  [2]string{"0,2","0"},
-			errCode: 2,
-		},
-	}
+	th := testhelper.CreateTestHelper[Input, any]()
+	th.AddTestcase([2]string{"1.2", "2"}, nil)
+	th.AddTestcase([2]string{"4.9", "5"}, nil)
+	th.AddTestcase([2]string{"-1", "2,"}, nil)
+	th.AddTestcase([2]string{"abc", "-5"}, nil)
+	th.AddTestcase([2]string{"0,2", "0"}, nil)
 
-	// Running the test cases
-	for _, tc := range testCases {
-		func() {
-			defer func(code int) {
-				r := recover()
-				if r == nil {
-					t.Errorf("The code did not panic")
+	for _, input := range th.GetInputs() {
+		didPanic, r := testhelper.ExpectPanic(t, func(){
+			sumStr(input[0], input[1])
+		})
+		if(didPanic){
+			correctType := reflect.TypeOf(r) == reflect.TypeOf(&DefMapInvalidArgs{})
+			if (!correctType) {
+				t.Fatalf("Not the expected error type")
+			} 
+			if (correctType){
+				rConverted, ok := r.(*DefMapInvalidArgs)
+
+				if !ok {
+					t.Fatalf("Conversion to the correct error type in defer function failed")
 				}
-
-				if reflect.TypeOf(r) != reflect.TypeOf(&DefMapInvalidArgs{}) {
-					t.Errorf("Not the expected error type")
-				} else {
-					rConverted, ok := r.(*DefMapInvalidArgs)
-
-					if !ok {
-						t.Errorf("Conversion to the correct error type in defer function failed")
-					}
-
-					if ok && rConverted.code != code {
-						t.Errorf("Panicked with the wrong code")
-					}
+				
+				if ok && rConverted.code != ExpectedErrorCode {
+					t.Fatalf("Panicked with the wrong code")
 				}
-			}(tc.errCode)
-			sumStr(tc.input[0], tc.input[1])
-		}()
+			}
+		}
 	}
 }
 
-func TestDefaultFuncHtmlBreaks(t *testing.T){
-	type TestCase struct {
-		input  string
-		output template.HTML
-	}
+func TestDefaultFuncHtmlBreaks(t *testing.T) {
+	type Input = string
+	type Output = template.HTML
+	
+	th := testhelper.CreateTestHelper[Input, Output]()
 
-	// Defining test cases
-	testCases := []TestCase{
-		{
-			input:  "This is a sentece\nAnother line starts",
-			output: "This is a sentece<br>Another line starts",
-		},
-		{
-			input:  "This is a sentece\nAnother line starts\nAnd another line",
-			output: "This is a sentece<br>Another line starts<br>And another line",
-		},
-		{
-			input:  "\nThis is a sentece\nAnother line starts\nAnd another line\\n",
-			output: "<br>This is a sentece<br>Another line starts<br>And another line\\n",
-		},
-	}
+	th.AddTestcase("This is a sentece\nAnother line starts", "This is a sentece<br>Another line starts")
+	th.AddTestcase("This is a sentece\nAnother line starts\nAnd another line", "This is a sentece<br>Another line starts<br>And another line")
+	th.AddTestcase("\nThis is a sentece\nAnother line starts\nAnd another line\\n", "<br>This is a sentece<br>Another line starts<br>And another line\\n")
 
-	// Running the test cases
-	for _, tc := range testCases {
-		output := htmlBreaks(tc.input)
-		// Check if the output matches the expected result
-		if !reflect.DeepEqual(tc.output, output) {
-			t.Errorf("Expected %v, got %v", tc.output, output)
-		}
+	for index, input := range th.GetInputs() {
+		output := htmlBreaks(input)
+		testhelper.ExpectEqual(t, output, th.GetOutput(index))
 	}
 }
